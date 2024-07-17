cd7f114698f864bf7038a6f32fe036d8a1bb4792
Chung-Bi <seokhyun5536@gmail.com>
2024-07-17 10:58:46 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a = 0;
+    long long int b = 0;
+    long long int c = 0;
+    
+    scanf("%lld %lld %lld", &a, &b, &c);
+    printf("%lld", a + b + c);
+    
+    return 0;
+}
